5d97befe3ebef4ad132ec0f6056c87a4f761559d
liujingjing <j6219519>
2022-10-14 17:00:05 +0800

    test

--- jd_wskey.py
+++ jd_wskey.py
@@ -291,7 +291,7 @@ def check_ck(ck):  # 方法 检查 Cookie有效性 使用变量传递 单次调�
         else:  # 判断分支
             if res.status_code == 200:  # 判断 JD_API 接口是否为 200 [HTTP_OK]
                 try:
-				    code = int(json.loads(res.text)['retcode'])  # 使用 Json模块对返回数据取值 int([retcode])
+                    code = int(json.loads(res.text)['retcode'])  # 使用 Json模块对返回数据取值 int([retcode])
                     if code == 0:  # 判断 code值
                        logger.info(str(pin) + ";状态正常\n")  # 标准日志输出
                        return True  # 返回 Bool类型 True
