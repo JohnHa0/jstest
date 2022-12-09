782a5646dccb5651c2cd5cd9fbe6f985ced8f1e8
liujingjing <j6219519>
2022-12-09 15:39:20 +0800

    test

--- jd_city.js
+++ jd_city.js
@@ -15,7 +15,12 @@ let exchangeFlag = $.isNode() ? (process.env.JD_CITY_EXCHANGE === "true" ? true 
 //IOS等用户直接用NobyDa的jd cookie
 let cookiesArr = [], cookie = '', message;
 let uuid, UA;
-$.shareCodes = []
+$.shareCodes = ['ou7yXKoFZGRGL17EBpfUh3c',
+  '-ryUBtdxNQYpPVe2Io24u_Vr5IqAq9Q',
+  '-ryUL_h4PB4xFmydMJG0u3AQDpfB9W4',
+  'oeDyWKkOb21DYherCpeJ9wsHT9YD',
+  '-ryUXa1fYWFBNRrEStLG90RDeqMqPgFC',
+  'dGFbjSa3sMPTtbx1k3Jlu-Bdk89aiQ8',]
 if ($.isNode()) {
   Object.keys(jdCookieNode).forEach((item) => {
     cookiesArr.push(jdCookieNode[item])
